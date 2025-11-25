c92f6521209a04e90544c0b58f1a285bea1c9e72
Takács Gábor <tgabesz@gmail.com>
2025-11-25 13:42:13 +0100

    Fix algorithm and setup text color in dark mode on training page

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.4tsdfjm5fao"
+    "revision": "0.k9jq0799avg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
