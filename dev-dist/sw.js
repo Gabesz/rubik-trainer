ac1e878b0d31695ef41f472a7a7750019d25bc06
Takács Gábor <tgabesz@gmail.com>
2026-03-23 11:40:47 +0100

    Navbar hamburger menu fixed

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.qbbdnedo5qg"
+    "revision": "0.g84jihpvbag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
